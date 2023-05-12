5110e4e91bed2238a4ef6ac4ebb1153e33a9ae87
Bernat <bernatcasasus97@gmail.com>
2023-05-12 21:12:58 +0200

    Add index.ts to expose Pages

---
+++ src/ui/components/pages/intex.ts
@@ -0,0 +1,6 @@
+import Character from './Character/Character';
+import Error from './Error/Error';
+import Home from './Home/Home';
+import Results from './Results/Results';
+
+export { Character, Error, Home, Results };
