4d499b52a2c4be962d0ad9ed9ac822b3f2945527
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-13 20:57:12 +0200

    Add @rmt/  alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@rmt/assets', replacement: path.resolve(__dirname, 'src/assets') },
+			{ find: '@rmt/routes', replacement: path.resolve(__dirname, 'src/routes/index.ts') },
+			{ find: '@rmt/pages', replacement: path.resolve(__dirname, 'src/ui/components/pages/index.ts') },
+			{ find: '@rmt/layout', replacement: path.resolve(__dirname, 'src/ui/components/layout/index.ts') },
+			{ find: '@rmt/atoms', replacement: path.resolve(__dirname, 'src/ui/components/atoms/index.ts') },
+			{ find: '@rmt/molecules', replacement: path.resolve(__dirname, 'src/ui/components/molecules/index.ts') },
+			{ find: '@rmt/organisms', replacement: path.resolve(__dirname, 'src/ui/components/organisms/index.ts') },
+		  ],
+	  },
 });
