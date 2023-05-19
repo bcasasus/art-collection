dff99865bea3aca51b4fb99c6a5bf4ee2ee71cd6
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-19 17:52:21 +0200

    Create Header component

--- src/ui/components/layout/Layout.tsx
+++ src/ui/components/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '@rmt/organisms';
 
 const Layout = (): JSX.Element => {
 	return (
 		<>
-			<div>Header</div>
+			<Header />
 			<Outlet />
 		</>
 	);
