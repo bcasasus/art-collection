d7dc88628390d5aa6555890a58bcda7973e4df77
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-19 17:46:47 +0200

    Create globalStyles and import use it in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from '@rmt/routes';
+import './ui/styles/globalStyles.css';
 
 const router = createBrowserRouter(routes);
 
