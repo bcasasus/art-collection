e8c24ffcb6ca2455cefd91fddaaa86d848e56145
Bernat <bernatcasasus97@gmail.com>
2023-05-19 20:27:27 +0200

    Add Card to Results page

--- src/ui/components/pages/Results/Results.tsx
+++ src/ui/components/pages/Results/Results.tsx
@@ -8,6 +8,8 @@ import {
 	setResultsAction,
 } from '@rmt/context';
 import { api } from '@rmt/api';
+import { Card } from '@rmt/atoms';
+import './results.css';
 
 const Results = () => {
 	const [state, dispatch] = useReducer(
@@ -26,7 +28,21 @@ const Results = () => {
 	return (
 		<ResultsStateContextProvider state={state}>
 			<ResultsDispatchContextProvider dispatch={dispatch}>
-				<div className="results-page">Results</div>
+				<div className="rmt-results-page">
+					<aside className="rmt-results-page__aside"></aside>
+					<main className="rmt-results-page__main">
+						{state.results.map(({ id, image, name }) => (
+							<Card key={id}>
+								<Card.Body>
+									<Card.Image src={image} alt={`A portrait of${name}`} />
+								</Card.Body>
+								<Card.Header>
+									<Card.Title>{name}</Card.Title>
+								</Card.Header>
+							</Card>
+						))}
+					</main>
+				</div>
 			</ResultsDispatchContextProvider>
 		</ResultsStateContextProvider>
 	);
