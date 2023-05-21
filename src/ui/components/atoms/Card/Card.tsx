8616b27bdb4695aba9ff123f15aaec5612f66e97
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-21 10:24:31 +0200

    Add className prop to Card and Header component

--- src/ui/components/atoms/Card/Card.tsx
+++ src/ui/components/atoms/Card/Card.tsx
@@ -8,15 +8,15 @@ import {
 } from './types';
 import './card.css';
 
-const Card: CardStructure = ({ children }) => (
-	<article className="rmt-card">{children}</article>
+const Card: CardStructure = ({ children, className = '' }) => (
+	<article className={`rmt-card ${className}`}>{children}</article>
 );
 
-Card.Header = ({ children }: HeaderProps) => (
-	<header className="rmt-card__header">{children}</header>
+Card.Header = ({ children, className = '' }: HeaderProps) => (
+	<header className={`rmt-card__header ${className}`}>{children}</header>
 );
 
-Card.Title = ({ children }: TitleProps) => {
+Card.Title = ({ children, className = '' }: TitleProps) => {
 	const titleClassName =
 		typeof children === 'string'
 			? children.length >= 20
@@ -24,19 +24,19 @@ Card.Title = ({ children }: TitleProps) => {
 				: 'rmt-card__title'
 			: 'rmt-card__title';
 
-	return <h2 className={titleClassName}>{children}</h2>;
+	return <h2 className={`${titleClassName} ${className}`}>{children}</h2>;
 };
 
-Card.Body = ({ children }: BodyProps) => (
-	<div className="rmt-card__body">{children}</div>
+Card.Body = ({ children, className = '' }: BodyProps) => (
+	<div className={`rmt-card__body ${className}`}>{children}</div>
 );
 
-Card.Image = ({ src, alt }: ImageProps) => (
-	<img className="rmt-card__img" src={src} alt={alt} />
+Card.Image = ({ src, alt, className = '' }: ImageProps) => (
+	<img className={`rmt-card__img ${className}`} src={src} alt={alt} />
 );
 
-Card.Footer = ({ children }: FooterProps) => (
-	<footer className="rmt-card__footer">{children}</footer>
+Card.Footer = ({ children, className = '' }: FooterProps) => (
+	<footer className={`rmt-card__footer ${className}`}>{children}</footer>
 );
 
 export default Card;
--- src/ui/components/organisms/Header/Header.tsx
+++ src/ui/components/organisms/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
 import { HeaderProps } from './types';
-import './header.css';
 import { Navbar } from '@rmt/molecules';
 import { NavLink } from 'react-router-dom';
 import rickAndMortyLogo from '/logo.svg';
+import './header.css';
 
-const Header: FC<HeaderProps> = ({ children }) => (
-	<header className="rmt-header">
+const Header: FC<HeaderProps> = ({ children, className = '' }) => (
+	<header className={`rmt-header ${className}`}>
 		<img src={rickAndMortyLogo} className="rmt-header__logo" alt="React logo" />
 		<Navbar>
 			<NavLink to="/">Home</NavLink>
