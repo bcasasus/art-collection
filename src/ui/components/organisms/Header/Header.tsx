503409159fa86ddefd14116f96a2ce8c87b5c70b
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-22 11:47:43 +0200

    Update Header height and spacing

--- src/ui/components/organisms/Header/Header.tsx
+++ src/ui/components/organisms/Header/Header.tsx
@@ -1,16 +1,22 @@
 import { FC } from 'react';
 import { HeaderProps } from './types';
 import { Navbar } from '@rmt/molecules';
-import { NavLink } from 'react-router-dom';
-import rickAndMortyLogo from '/logo.svg';
+import rickAndMortyLogo from '@rmt/assets/logo.svg';
 import './header.css';
 
 const Header: FC<HeaderProps> = ({ children, className = '' }) => (
 	<header className={`rmt-header ${className}`}>
-		<img src={rickAndMortyLogo} className="rmt-header__logo" alt="React logo" />
+		<div className="rmt-header__section">
+			<img
+				src={rickAndMortyLogo}
+				className="rmt-header__logo"
+				alt="React logo"
+			/>
+			The Rick and Morty Tracker
+		</div>
 		<Navbar>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/results">Results</NavLink>
+			<Navbar.Link to="/">Home</Navbar.Link>
+			<Navbar.Link to="/results">Results</Navbar.Link>
 		</Navbar>
 		{children}
 	</header>
