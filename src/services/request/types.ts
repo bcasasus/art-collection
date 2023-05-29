e2f1d62d5f5941375a9926c09fad92536464ec34
bernat casasus <bernat.casasus@MAC-1901-064.local>
2023-05-29 14:35:27 +0200

    Add CharacterFilters, CharacterParams, ParamsStructure, Params and ParamKeys

--- src/services/request/types.ts
+++ src/services/request/types.ts
@@ -2,17 +2,23 @@ export interface Endpoints {
 	[key: string]: string;
 }
 
-export interface CharacterParams {
+export interface CharacterFilters {
 	name?: string;
 	status?: string;
 	gender?: string;
 }
 
-export type Params = {
-	[key in keyof CharacterParams as string]?: string | number;
+export type CharacterParams = {
+	[key in keyof CharacterFilters as string]?: string | number;
 };
 
-export type ParamsKeys = keyof Params;
+export type ParamsStructure = {
+	[key: string]: string | number;
+};
+
+export type Params = Partial<ParamsStructure>;
+
+export type ParamsKeys = keyof ParamsStructure;
 
 export interface CallApiStructure {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
