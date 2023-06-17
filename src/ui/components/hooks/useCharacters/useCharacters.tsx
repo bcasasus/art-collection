1cb2815cc451e1cca4dc11032e1bc5f71b287eab
Bernat <bernatcasasus97@gmail.com>
2023-06-17 18:48:13 +0200

    Refactor useCharacters to get characters from ResultsContext and setCharacters form ResultsDispatchContext

--- src/ui/components/hooks/useCharacters/useCharacters.tsx
+++ src/ui/components/hooks/useCharacters/useCharacters.tsx
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
 import { api } from '@rmt/api';
 import { CharacterParams, mapCharacters } from '@rmt/services';
-import { Character, CharactersFetchInformation } from '@rmt/model';
+import { CharactersFetchInformation } from '@rmt/model';
+import { useResultsContext } from '../../pages/Results/context/useResultsContext';
+import { useResultsDispatchContext } from '../../pages/Results/context/useResultsDispatchContext';
 
 const CHARACTER_RESULTS_INFORMATION_INITIAL_STATE = {
 	totalPages: 0,
@@ -9,27 +11,31 @@ const CHARACTER_RESULTS_INFORMATION_INITIAL_STATE = {
 };
 
 export const useCharacters = () => {
-	const [characters, setCharacters] = useState<Character[]>([]);
+	// const [characters, setCharacters] = useState<Character[]>([]);
+	const { characters } = useResultsContext();
+	const { setCharacters } = useResultsDispatchContext();
 	const [charactersFetchInformation, setCharactersFetchInformation] =
 		useState<CharactersFetchInformation>(
 			CHARACTER_RESULTS_INFORMATION_INITIAL_STATE
 		);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-	const fetchCharacters = useCallback(async (params: CharacterParams) => {
-		try {
-			setIsLoading(true);
-			const response = await api.fetchCharacters(params);
-			const characters = mapCharacters(response);
+	const fetchCharacters = useCallback(
+		async (params: CharacterParams) => {
+			try {
+				setIsLoading(true);
+				const response = await api.fetchCharacters(params);
+				const characters = mapCharacters(response);
 
-			setCharacters(characters.results);
-			setCharactersFetchInformation(characters.resultsInfo);
-			setIsLoading(false);
-		} catch {
-			setCharacters([]);
-			setIsLoading(false);
-		}
-	}, []);
+				setCharacters({ characters: characters.results });
+				setCharactersFetchInformation(characters.resultsInfo);
+				setIsLoading(false);
+			} catch {
+				setIsLoading(false);
+			}
+		},
+		[setCharacters]
+	);
 
 	useEffect(() => {
 		(async () => {
