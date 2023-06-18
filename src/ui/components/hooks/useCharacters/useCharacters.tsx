940eaece7ba18dc151fc5d69d17c532207383254
Bernat <bernatcasasus97@gmail.com>
2023-06-18 19:19:12 +0200

    Refactor useCharacters hook to retrive information fron useResultsDispatchContext and to only return fetchCharacters function to call the api

--- src/ui/components/hooks/useCharacters/useCharacters.tsx
+++ src/ui/components/hooks/useCharacters/useCharacters.tsx
@@ -1,52 +1,41 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { api } from '@rmt/api';
 import { CharacterParams, mapCharacters } from '@rmt/services';
-import { CharactersFetchInformation } from '@rmt/model';
-import { useResultsContext } from '../../pages/Results/context/useResultsContext';
 import { useResultsDispatchContext } from '../../pages/Results/context/useResultsDispatchContext';
 
-const CHARACTER_RESULTS_INFORMATION_INITIAL_STATE = {
-	totalPages: 0,
-	totalCharactersCount: 0,
-};
-
 export const useCharacters = () => {
-	// const [characters, setCharacters] = useState<Character[]>([]);
-	const { characters } = useResultsContext();
-	const { setCharacters } = useResultsDispatchContext();
-	const [charactersFetchInformation, setCharactersFetchInformation] =
-		useState<CharactersFetchInformation>(
-			CHARACTER_RESULTS_INFORMATION_INITIAL_STATE
-		);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const {
+		setCharacters,
+		setCharactersCount,
+		setPaginationTotalPages,
+		setIsLoading,
+	} = useResultsDispatchContext();
 
 	const fetchCharacters = useCallback(
 		async (params: CharacterParams) => {
 			try {
-				setIsLoading(true);
+				setIsLoading({ isLoading: true });
+
 				const response = await api.fetchCharacters(params);
 				const characters = mapCharacters(response);
 
 				setCharacters({ characters: characters.results });
-				setCharactersFetchInformation(characters.resultsInfo);
-				setIsLoading(false);
+				setCharactersCount({
+					charactersCount: characters.resultsInfo.totalCharactersCount,
+				});
+				setPaginationTotalPages({
+					totalPages: characters.resultsInfo.totalPages,
+				});
+				setIsLoading({ isLoading: false });
 			} catch {
-				setIsLoading(false);
+				setCharacters({ characters: [] });
+				setIsLoading({ isLoading: false });
 			}
 		},
-		[setCharacters]
+		[setCharacters, setCharactersCount, setPaginationTotalPages, setIsLoading]
 	);
 
-	useEffect(() => {
-		(async () => {
-			await fetchCharacters({});
-		})();
-	}, [fetchCharacters]);
-
 	return {
-		characters,
-		charactersFetchInformation,
-		isLoading,
 		fetchCharacters,
 	};
 };
