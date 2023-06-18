73a6fdcdf18446eac5d9cdc9d68c7384ead423ca
Bernat <bernatcasasus97@gmail.com>
2023-06-18 19:30:01 +0200

    Render CharactersInformation, CharactersList and CharactersPagination components

--- src/ui/components/pages/Results/Results.tsx
+++ src/ui/components/pages/Results/Results.tsx
@@ -1,86 +1,25 @@
-import { useCharacters } from '@rmt/hooks';
-import { CardSkeleton, Filters, Card, Pagination } from '@rmt/molecules';
-import { useCallback, useRef } from 'react';
-import { CharacterParams } from '@rmt/services';
+import { CharactersPagination, Filters } from '@rmt/molecules';
+import { CharactersInformation } from '@rmt/atoms';
+import { ResultsContextProvider } from './context/resultsContextProvider';
+import { CharactersList } from '../../organisms/CharactersList/CharactersList';
 import './results.css';
 
 const Results = () => {
-	const { characters, charactersFetchInformation, isLoading, fetchCharacters } =
-		useCharacters();
-
-	const resultsMainRef = useRef<HTMLElement>(null);
-
-	const scrollToMain = () =>
-		window.scrollTo({
-			top: resultsMainRef?.current?.offsetTop,
-			left: 0,
-			behavior: 'smooth',
-		});
-
-	const handleChangePagination = useCallback(
-		async (currentPage: number) => {
-			scrollToMain();
-			await fetchCharacters({ page: currentPage });
-		},
-		[fetchCharacters]
-	);
-
-	const handleSubmitFilters = async (filters: CharacterParams) => {
-		scrollToMain();
-		await fetchCharacters(filters);
-	};
-
 	return (
-		<div className="rmt-results-page">
-			<div className="rmt-results-page__container">
-				<aside className="rmt-results-page__aside">
-					<Filters onSubmit={handleSubmitFilters} />
-				</aside>
-				<main className="rmt-results-page__main" ref={resultsMainRef}>
-					<div className="rmt-results-page__results-info">
-						{!isLoading && !characters.length ? (
-							<span className="rmt-results-page__text">No results found.</span>
-						) : (
-							<span className="rmt-results-page__text">
-								Found a total of{' '}
-								{charactersFetchInformation.totalCharactersCount} characters.
-							</span>
-						)}
-					</div>
-
-					<ul className="rmt-results-page__list">
-						{isLoading &&
-							Array.from({ length: 20 }).map((_element, index) => (
-								<li key={index} className="rmt-results-page__list-item">
-									<CardSkeleton />
-								</li>
-							))}
-						{!isLoading &&
-							!!characters.length &&
-							characters.map(({ id, image, name }) => (
-								<li key={id} className="rmt-results-page__list-item">
-									<Card>
-										<Card.Body>
-											<Card.Image
-												src={image}
-												alt={`A portrait of ${name} from Rick and Morty.`}
-											/>
-										</Card.Body>
-										<Card.Header>
-											<Card.Title>{name}</Card.Title>
-										</Card.Header>
-									</Card>
-								</li>
-							))}
-					</ul>
-
-					<Pagination
-						totalPages={charactersFetchInformation.totalPages}
-						onChangePage={handleChangePagination}
-					/>
-				</main>
+		<ResultsContextProvider>
+			<div className="rmt-results-page">
+				<div className="rmt-results-page__container">
+					<aside className="rmt-results-page__aside">
+						<Filters />
+					</aside>
+					<main className="rmt-results-page__main">
+						<CharactersInformation />
+						<CharactersList />
+						<CharactersPagination />
+					</main>
+				</div>
 			</div>
-		</div>
+		</ResultsContextProvider>
 	);
 };
 
